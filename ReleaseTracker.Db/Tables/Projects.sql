ef20f15410dceae34c5c327d5c15d48f355d14eb
arminkazic <armin_kazic@outlook.com>
2015-11-26 20:11:52 +0100

    Data base

---
+++ ReleaseTracker.Db/Tables/Projects.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Projects](
+	[Id] [bigint] IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	[Name] [nvarchar](150) NOT NULL,
+	[Description] [text] NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+
