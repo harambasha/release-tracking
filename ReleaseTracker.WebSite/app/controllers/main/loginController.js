4f531553e42d140326393c23c23a6c631fb323b2
iharambasic <ismirha@maestralsolutions.com>
2015-12-02 17:37:26 +0100

    Validate username and password

--- ReleaseTracker.WebSite/app/controllers/main/loginController.js
+++ ReleaseTracker.WebSite/app/controllers/main/loginController.js
@@ -1,19 +1,13 @@
 ﻿'use strict';
 app.controller('loginController', function ($scope, $http) {
-    $scope.valid = true;
+    $scope.invalidLogin = false;
 
+    // koristi servise i rute MOLIM TE
     $scope.login = function () {
-        var user = new Object();
         $http.get("http://localhost:60126/api/Users?email=" + $scope.email + "&password=" + $scope.password)
         .then(function (response) {
-            user = response.data;
+            $scope.user = response.data;
+            $scope.invalidLogin = $scope.user != null;
         });
-        alert(user.Email);
-        if (user != null) {
-            $scope.valid = true;
-        }
-        else {
-            $scope.valid = false;
-        }
     }
 });
